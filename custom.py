4282e38d50866bae277479d609466e623ea48a14
vava24680 <9203700@gmail.com>
2019-02-24 14:44:38 +0800

    Door switch

--- custom.py
+++ custom.py
@@ -1,16 +1,21 @@
 import DAN
 
-ServerIP = 'http://IP:9999'   #=None:AutoSearch, or ='IP':Connect to this IP
+ServerURL = 'http://DomainName:9999'   #=None:AutoSearch, or ='IP':Connect to this IP
 #ServerIP = 'https://DomainName'   #=None:AutoSearch, or ='IP':Connect to this IP
 Comm_interval = 0.5 # unit:second
 
 def profile_init():
-    DAN.profile['dm_name']='Windows'
-    DAN.profile['d_name']=DAN.profile['dm_name']+'.'+DAN.get_mac_addr()[-4:]
+    DAN.profile['dm_name'] = 'Door'
+    DAN.profile['d_name'] = DAN.profile['dm_name'] + '.' + DAN.get_mac_addr()[-4:]
 
 def odf():  # int only
+    # Return a list of tuples
+    # Each tuple consists of 3 elements,
+    # first is the feature name
+    # second is the index of this tuple in this list
+    # third is the key name of one key-value pair that will be used in bridge
     return [
-        ('Switch', 0, 'Switch'),
+        ('Open', 0, 'Open')
     ]
 
 def idf():
